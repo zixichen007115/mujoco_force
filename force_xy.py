2f2e3a9726db56ed20afa9df324be2eed1493520
zixichen007115 <75701303+zixichen007115@users.noreply.github.com>
2022-01-13 10:01:19 +0800

    Update force_xy.py

--- force_xy.py
+++ force_xy.py
@@ -80,78 +80,18 @@ def save(force_list,pos_list,x,y,z):
 
 while True:
 
-    # simulate()
 
 
     # move
     for z in z_list:
         for x in x_list:
-            
-
-            # move(x,y,-0.01)
-
-
             # probe
             for y in y_list:
-                # move(x,y,z)
-
-                # if x >0.001 and y>0.001 and z<0.01:
-
                 force_list,pos_list = move(force_list,pos_list,x,y,z)
-                print('here')
                 force = sim.data.sensordata[:3]
                 pos = sim.data.sensordata[3:]
                 pos = [np.round(pos[0],4),np.round(pos[1],4),np.round(pos[2],5)]
                 print("time:{}, force: {}, position: {}".format(t,force[2],pos))
-            # move(x,y,-0.01)
-            # move(x,y,0)
     np.savez('data',force=force_list, pos=pos_list)
     sys.exit()
-
-
-
-        # simulate()
-
-        # # sensor feedback
-        # sim.data.get_sensor('F/T')
-        # force = sim.data.sensordata[:3]
-        # pos = sim.data.sensordata[3:]
-
-        # # if t<601:
-        # #         force_list[t-1]=force
-        # #         pos_list[t-1] = pos
-        # # elif save =='False':
-        # #         save = 'True'
-        # #         np.savez('data',force=force_list, pos=pos_list)
-
-        # # gear = 10
-        # if t<0.01/0.001*10:
-        #         # t:0-100, ctrl:0 -> -0.1
-        #         sim.data.ctrl[2] = -t*0.001
-        # elif t<0.01/0.001*10+100:
-        #         # t:100-200, ctrl:-0.1
-        #         sim.data.ctrl[2] = -0.1
-        # elif t<0.01/0.001*10+100+100:
-        #         # t:200-300, ctrl:-0.1 -> -0.11
-        #         sim.data.ctrl[2] = -0.1-(t-200)*0.0001
-        # elif t<0.01/0.001*10+100+100+100:
-        #         # t:300-400, ctrl:-0.11
-        #         sim.data.ctrl[2] = -0.11
-        # elif t<0.01/0.001*10+100+100+100+100:
-        #         # t:400-500, ctrl:-0.11 -> -0.12
-        #         sim.data.ctrl[2] = -0.11-(t-400)*0.0001
-        # else:
-        #         sim.data.ctrl[2] = -0.12
-
-
-
-        # # if pos>0.045 and flag == "no_touch":
-        # #         sim.data.ctrl[0] = -t*0.001
-        # # else:
-        # #         flag="touch"
-
-
-
-
-        # print("time:{}, force: {}, position: {}".format(t,force,pos))
 
